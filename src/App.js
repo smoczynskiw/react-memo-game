970af3463e9f049c13163d2b6cdc10570f552798
wixor <smoczynski.w@gmail.com>
2020-04-27 09:38:23 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Game from './Game'
 
 function App() {
+
+  const style = {
+    width: '100vw',
+    height: '100vh',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    fontFamily: 'Roboto',
+    fontWeight: '300',
+    backgroundColor: 'rgba(0,0,0)'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={style}>
+      <Game />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
