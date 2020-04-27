ea66c588c6462bdbc49a0da4bf7ef14fa5392e08
wixor <smoczynski.w@gmail.com>
2020-04-27 18:55:18 +0200

    Updates

---
+++ precache-manifest.78f9d040f619818b433fcefda55b073f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6759bcff4031631dcd1fef3a33ad32df",
+    "url": "/react-memo-game/index.html"
+  },
+  {
+    "revision": "367ecc837c7ee75ef93e",
+    "url": "/react-memo-game/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "03ac2bd0c603faef5e52",
+    "url": "/react-memo-game/static/js/2.150760b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-memo-game/static/js/2.150760b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "367ecc837c7ee75ef93e",
+    "url": "/react-memo-game/static/js/main.d65dbaae.chunk.js"
+  },
+  {
+    "revision": "1891eb5c12d0c5aa763b",
+    "url": "/react-memo-game/static/js/runtime-main.4bd1c060.js"
+  },
+  {
+    "revision": "cd5f27f287693dab570639e1253d8092",
+    "url": "/react-memo-game/static/media/1.cd5f27f2.jpg"
+  },
+  {
+    "revision": "32d8748629cab06c1ecb21ef17b65ffb",
+    "url": "/react-memo-game/static/media/2.32d87486.jpg"
+  },
+  {
+    "revision": "baaa08e4b55d05e1c8fd80a1982295b6",
+    "url": "/react-memo-game/static/media/3.baaa08e4.jpg"
+  },
+  {
+    "revision": "9713d1f63cdb9830f2d150498a36cca5",
+    "url": "/react-memo-game/static/media/4.9713d1f6.jpg"
+  },
+  {
+    "revision": "8344ca5d9671bc882daabffb6997bd1e",
+    "url": "/react-memo-game/static/media/5.8344ca5d.jpg"
+  },
+  {
+    "revision": "818860947af42877f8541e502c2496be",
+    "url": "/react-memo-game/static/media/6.81886094.jpg"
+  },
+  {
+    "revision": "dd8cc4a039baf00960fa3da73339271b",
+    "url": "/react-memo-game/static/media/7.dd8cc4a0.jpg"
+  },
+  {
+    "revision": "94eb3343c9a27bab0c75688b019840a7",
+    "url": "/react-memo-game/static/media/8.94eb3343.jpg"
+  },
+  {
+    "revision": "f7583149e7ab2d0b00bfc36eaa8bdb24",
+    "url": "/react-memo-game/static/media/faceDown.f7583149.jpg"
+  }
+]);
